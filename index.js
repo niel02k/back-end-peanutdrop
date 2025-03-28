769ea969fd0efcda5a16e28b51b641c7f29b55c3
niel02k <nielfreitasfree@gmail.com>
2025-03-28 11:41:25 -0300

    28/03 11:41

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require ('express')
+require('dotenv').config();
+const express = require('express'); 
+const cors = require('cors');
 
-const app = express();
 
-const porta =333;
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+
+const porta = process.env.PORT || 3333;
 
 app.listen(porta, () => {
-    console.log(`Servidor iniciado na porta ${porta}`);
-
+    console.log(`Servidor iniciado em http://localhost:${porta}`);
 });
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
+
