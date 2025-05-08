932b7fdbdb0291244550fe4777af19a274c0a92f
niel02k <nielfreitasfree@gmail.com>
2025-05-08 11:45:23 -0300

    08/05

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -3,10 +3,18 @@ const db = require('../dataBase/connection');
 module.exports = {
     async listarUsuarios(request, response) {
         try {
+
+            const sql= `SELECT usu_id,usu_tipo_usuario,usu_nome,usu_documento,usu_email, usu_senha,usu_endereco,usu_telefone ,usu_data_cadastro FROM USUARIOS;`;
+
+
+            const [rows] = await db.query(sql);
+            const nRegistros = rows.length;
+            
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de usuários', 
-                dados: null
+                nRegistros,
+                dados: rows
             });
         } catch (error) {
             return response.status(500).json({
