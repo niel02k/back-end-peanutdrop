de3b87cc8b9a06ed622d14a9c3d868200888162c
niel02k <nielfreitasfree@gmail.com>
2025-05-16 10:50:48 -0300

    16/05

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,8 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require ('express');
+const router = express.Router();
 
-const negociacoesController = require('../controllers/negociacoes'); 
-const mensagemsController = require('../controllers/mensagem'); 
-const usuariosController = require('../controllers/usuarios'); 
+const RotasMarcos = require ('./routes-marcos');
 
-router.get('/usuarios', usuariosController.listarUsuarios); 
-router.post('/usuarios', usuariosController.cadastrarUsuarios); 
-router.patch('/usuarios/:id', usuariosController.editarUsuarios); 
-router.delete('/usuarios/:id', usuariosController.apagarUsuarios); 
-
-
-router.get('/negociacoes', negociacoesController.listarNegociacoes); 
-router.post('/negociacoes', negociacoesController.cadastrarNegociacoes); 
-router.patch('/negociacoes/:id', negociacoesController.editarNegociacoes); 
-router.delete('/negociacoes/:id', negociacoesController.apagarNegociacoes); 
-
-router.get('/mensagem', mensagemsController.listarMensagem); 
-router.post('/mensagem', mensagemsController.cadastrarMensagem); 
-router.patch('/mensagem/:id', mensagemsController.editarMensagem); 
-router.delete('/mensagem/:id', mensagemsController.apagarMensagem); 
+router.use('/' , RotasMarcos );
 
 module.exports = router;
